ac8864584fda40b5565a6798f8aebc187dbebdba
David Kamer <me@davidkamer.com>
2023-10-01 19:52:10 -0400

    v1 placement site

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import { useSDK } from '@metamask/sdk-react'
+import React, { useState } from 'react'
+import abi from './gitarg.json'
+import Web3 from 'web3'
 import './App.css';
+import logo from './img/SVG/argctl.svg';
 
+const contract = '0xb85C685226095d20EA648C35B9CCE6C1556006A5'
 function App() {
+  const { sdk, connected, connecting, provider, chainId } = useSDK()
+
+  const [account, setAccount] = useState()
+  const connect = async () => {
+    try {
+      const accounts = await sdk.connect()
+      setAccount(accounts[0]);
+    } catch(err) {
+      console.warn(`failed to connect..`, err);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Interface Tar Git, Git Tar Fascinator
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={connect}>connect</button>
+        {account && <div> {account} </div>}
+        <p>test your wallet, teller wait start</p>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MetaMaskProvider } from '@metamask/sdk-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <MetaMaskProvider debug={false} sdkOptions={{
+        logging:{
+          developerMode: false,
+        },
+        communicationServerUrl: process.env.REACT_APP_COMM_SERVER_URL,
+        checkInstallationImmediately: false, // This will automatically connect to MetaMask on page load
+        dappMetadata: {
+          name: "argctl",
+          url: window.location.host,
+        }
+      }}>
+      <App />
+    </MetaMaskProvider>
   </React.StrictMode>
 );
 
